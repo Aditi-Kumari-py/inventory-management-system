e4c04e7b5162c8cd170b46a6c192000538402e5c
Aditi-Kumari-py <aditikumari.work@gmail.com>
2026-07-19 00:05:41 +0530

    Configure backend for cloud deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,19 @@ dotenv.config();
 import app from "./app.js";
 import pool from "./config/db.js";
 
+// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 5000;
+
+
 
 async function startServer() {
   try {
     const result = await pool.query("SELECT NOW()");
     console.log("✅ PostgreSQL connected:", result.rows[0].now);
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (error) {
     console.error("❌ Database connection failed");
     console.error(error);
